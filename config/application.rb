5dae0b56a191efa81a59df1df4993ca73387d353
vishwas patil <vishpatil145@gmail.com>
2026-04-04 17:46:00 +0530

    Change middleware location

--- config/application.rb
+++ config/application.rb
@@ -3,14 +3,14 @@ require "rails/all"
 
 Bundler.require(*Rails.groups)
 
-require_relative "../app/middleware/rate_limiter"
+require_relative "../lib/middleware/rate_limiter"
+
 module PhiVaultApi
   class Application < Rails::Application
     config.load_defaults 8.1
 
     config.api_only = true
     config.active_job.queue_adapter = :sidekiq
-    config.autoload_paths.delete("#{root}/app/middleware")
 
     config.middleware.use Middleware::RateLimiter unless Rails.env.test?
   end
